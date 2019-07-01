77e560ade62ff8e9d135008eacd6ca98eeb177ce
guillaumeb74 <guillaume.brut@viacesi.fr>
2019-07-01 13:53:40 +0200

    ibfv

--- new.py
+++ new.py
@@ -1,75 +1,104 @@
+
 from random import randint
 
-print ("Entrez la taille de la matrice (attention + d'1 minute d'attente apres 500) ")
-taille = input()
-
-taille = int(taille)
-
-pointTable = []
-
-stop = False
-again = True
-
-for x in range(taille):
+class CreatePays:
     
-    pointTable.append([])
     
-    again = True
-    stop = False
     
-    print(pointTable)
+    def __init__(self, name):
+        self.name = name
     
-    while again == True:
+        
+        
+        self.pointTable = []
+    
+    
+    
+        print ("Entrez la taille de la matrice (attention + d'1 minute d'attente apres 500) ")
+        taille = input()
+        
+        taille = int(taille)
+        
+        pointTable = self.pointTable
         
         stop = False
-    
-        abscisse = randint(0, 99)
-        ordonee = randint(0, 99)
+        again = True
         
-        print(abscisse, ordonee)
-        
-        if len(pointTable) > 1:
-    
-            while stop == False:
-                for y in (pointTable):
+        for x in range(taille):
+            
+            pointTable.append([])
+            
+            again = True
+            stop = False
+            
+            print(pointTable)
+            
+            while again == True:
                 
-                    print("+++++++++++++++++++++++++++++++++++++")
-        
-                    print("test",y);
-                    
-                    if len(y) > 1:
+                stop = False
+            
+                abscisse = randint(0, 99)
+                ordonee = randint(0, 99)
                 
-                        if (abscisse == y[0]) and (ordonee == y[1]):
+                print(abscisse, ordonee)
                 
-                            print("-----------------------------")
-                            
-                            stop = True
-                            
-                            break
-                            
-                            
+                if len(pointTable) > 1:
+            
+                    while stop == False:
+                        for y in (pointTable):
                         
-               
-                if stop == False :
-                    again = False
+                            print("+++++++++++++++++++++++++++++++++++++")
                 
-                if stop == False and again == False:
-    
-                    stop = True
-                 
+                            print("test",y);
+                            
+                            if len(y) > 1:
+                        
+                                if (abscisse == y[0]) and (ordonee == y[1]):
+                        
+                                    print("-----------------------------")
+                                    
+                                    stop = True
+                                    
+                                    break
+                                    
+                                    
+                                
+                       
+                        if stop == False :
+                            again = False
+                        
+                        if stop == False and again == False:
+            
+                            stop = True
+                         
+                            pointTable[x].append(abscisse)
+                            pointTable[x].append(ordonee)
+                            
+                    print(stop)
+                            
+                else:
+                    
+                    print("willian")
+                    
                     pointTable[x].append(abscisse)
                     pointTable[x].append(ordonee)
                     
-            print(stop)
+                    again = False
                     
-        else:
             
-            print("willian")
+        print(pointTable)
+        
+        x_number_list = []
+        y_number_list = []
+        
+        for x in (pointTable):
             
-            pointTable[x].append(abscisse)
-            pointTable[x].append(ordonee)
-            
-            again = False
-            
-    
-print(pointTable)
+            x_number_list.append(x[0])
+            y_number_list.append(x[1])
+
+
+
+p1 = CreatePays("pays1")
+
+print(p1.pointTable[5])
+
---
+++ ville.py
@@ -0,0 +1,22 @@
+class CreateVille:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+    
+    def get_x(self):
+        return self.name
+
+    def set_x(self, x):
+        self.name = x
+    
+    #self.ville = [self.name, self.x, self.y]
+
+
+ville1 = CreateVille("Tamere", 10, 50)
+
+print (ville1.get_x())
+
+ville1.set_x("ouaf")
+
+print (ville1.get_x())
