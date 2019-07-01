a42a7a32914a350c124b5b8aebdaa9017fcf0d95
guillaumeb74 <guillaume.brut@viacesi.fr>
2019-07-01 15:00:03 +0200

    main 2.0

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from pays import *
 
 
-print ("Entrez la taille de la matrice")
-nombreVilles = input()
+def main():
 
-p1 = Pays("pays1")
-p1.createPays(nombreVilles)
+    p1 = Pays("pays1")
+    
+    print ("Entrez le nombre de ville souhaités pour le pays (Optimalement le nombre de villes doit etre inferieur à 5000 ou 9000 au max)")
+    nombreVilles = int(input())
+    
+    if nombreVilles > 9000:
+        print("Le nombre de villes doit etre inferieur à 9000")
+        print("")
+        main()
+    else:
+    
+        p1.createPays(nombreVilles)
+    
+        for x in p1.pays:
+            print(x.name, x.x, x.y)
 
-for x in p1.pays:
-    print(x.name, x.x, x.y)
+main()
