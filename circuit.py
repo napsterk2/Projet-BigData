da63e09630fec7fb337401e1730d96e64d3524e0
WilliamNunesse <william.nunesse@viacesi.fr>
2019-07-01 16:08:50 +0200

    cache

---
+++ circuit.py
@@ -0,0 +1,9 @@
+class Circuit:
+	
+	def __init__(self, gestionnaireCircuits, trajet=None):
+		self.gestionnaireCircuits = gestionnaireCircuits
+		self.trajet = []
+		self.noteCircuit = 0
+		self.distance = 0
+		
+		
